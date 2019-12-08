0a014577def0e671b95ae31bc46fd190e6d4a369
Sangwan Jeon <maxswjeon@naver.com>
2019-12-09 01:48:08 +0900

    Implement Thread Class
    Delete Copy/Move Constructor
    Pure Virtual Function Thread:run()
    Add ThreadStatus

--- Brick/include/Threads/Thread.h
+++ Brick/include/Threads/Thread.h
@@ -2,13 +2,30 @@
 
 #include <thread>
 
+enum class ThreadStatus
+{
+	RUNNING,
+	STOPPED,
+	STOPPING
+};
+
 class Thread
 {
 private:
 	std::thread _thread;
-
+	ThreadStatus _status;
+	
 public:
+	Thread();
+	virtual ~Thread();
+
+	Thread(const Thread& other) = delete;
+	Thread(Thread&& other) noexcept = delete;
+	Thread& operator=(const Thread& other) = delete;
+	Thread& operator=(Thread&& other) noexcept = delete;
+	
 	void start();
 	void stop();
-};
+	virtual void run();
 
+};
