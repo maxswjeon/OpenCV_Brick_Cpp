14dd3ad17b3283100377ecb821c58ac03fb4be64
Sangwan Jeon <maxswjeon@naver.com>
2019-12-09 00:59:08 +0900

    Add Webcam Thread

---
+++ Brick/include/Threads/Webcam.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Threads/Thread.h"
+
+#include <opencv2/opencv.hpp>
+
+class Webcam : public Thread
+{
+	
+public:
+	Webcam();
+};
+
---
+++ Brick/src/Threads/Webcam.cpp
@@ -0,0 +1,5 @@
+#include "Threads/Webcam.h"
+
+Webcam::Webcam()
+{
+}
